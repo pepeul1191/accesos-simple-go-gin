60b7a29a7b4dce504841dff28612e5ac18a5de2a
Pepe Valdivia <jvaldivia@softweb.pe>
2018-07-10 12:35:16 -0500

    Primer middleware beforeAll

--- app.go
+++ app.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/ginv2/configs"
 )
@@ -15,14 +17,25 @@ func GetPong(c *gin.Context) {
 
 func main() {
 	r := gin.Default()
-	configs.SetConstants()
+	// cargando constantes
+	//configs.SetConstants()
+	// configuraciones de vistas y archivos estáticos
+	r.LoadHTMLGlob("templates/**/*")
 	r.Static("/public", "./public")
 	r.StaticFile("/favicon.ico", "./public/favicon.ico")
+	// middleware personalizado
+	r.Use(configs.BeforeAll())
+	// rutas
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong??",
 		})
 	})
 	r.GET("/pong", GetPong)
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "home/index.tmpl", gin.H{
+			"title": "Home",
+		})
+	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
