b75a84ec317945fe201487cf2628a9331d6f5aa9
Pepe Valdivia <jvaldivia@softweb.pe>
2018-07-10 12:22:36 -0500

    Archivo de constantes

--- app.go
+++ app.go
@@ -1,12 +1,16 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ginv2/configs"
+)
 
 func main() {
 	r := gin.Default()
+	configs.SetConstants()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "pong??",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
---
+++ configs/constants.go
@@ -0,0 +1,8 @@
+package configs
+
+var Constants = make(map[string]string)
+
+func SetConstants() {
+	Constants["BASE_URL"] = "http://localhost:8080/"
+	Constants["STATIC_URL"] = "http://localhost:8080/public/"
+}
