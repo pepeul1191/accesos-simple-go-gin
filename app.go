25595bc1fbf6b8e4c84c783a5fd3e31507215a03
Pepe Valdivia <jvaldivia@softweb.pe>
2018-07-10 12:24:34 -0500

    Archivos estáticos

--- app.go
+++ app.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	r := gin.Default()
 	configs.SetConstants()
+	r.Static("/public", "./public")
+	r.StaticFile("/favicon.ico", "./public/favicon.ico")
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong??",
