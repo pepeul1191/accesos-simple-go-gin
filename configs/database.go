94670aa49710c7d6215581025cf4a24a300ce177
Pepe Valdivia <jvaldivia@softweb.pe>
2018-10-08 17:44:48 -0500

    Migración create_user_state

--- configs/database.go
+++ configs/database.go
@@ -2,11 +2,11 @@ package configs
 
 import (
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func Database() *gorm.DB {
-	db, err := gorm.Open("sqlite3", "db/ubicaciones.db")
+	db, err := gorm.Open("mysql", "root:123@/access?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic("failed to connect database")
 	}
